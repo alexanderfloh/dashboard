995f32cd974be8241866c81067000de083e4cc5f
alexanderfloh <alexander.floh@gmail.com>
2014-05-24 12:25:34 +0200

    extended mock response to include commits

--- app/util/MockResponseGenerator.scala
+++ app/util/MockResponseGenerator.scala
@@ -8,12 +8,42 @@ import scala.io.Codec
 
 object MockResponseGenerator {
   def apply(url: String): String = url match {
-    case "http://lnz-bobthebuilder/hudson/job/SilkTest%20CI" =>
+    case "http://lnz-bobthebuilder/hudson/job/SilkTest%20CI" => {
+      val changeSetItems =
+        """[
+      {
+        "affectedPaths" : [
+          "OpenAgent/SrcJava/plugins/com.borland.fastxd.integration.xp/src/com/borland/fastxd/integration/xp/functions/AgentClassFunction.java",
+          "SilkTestClassic/src/startup/winclass.inc"
+        ],
+        "author" : {
+          "absoluteUrl" : "http://lnz-bobthebuilder/hudson/user/MichaelG",
+          "fullName" : "Michael Gehmayr"
+        },
+        "commitId" : "76695",
+        "timestamp" : 1400237433062,
+        "date" : "2014-05-16T10:50:33.062367Z",
+        "msg" : "+ Agent.ShutDown() method for 4Test (US9116 US - RFE - Expose stop/start Open Agent)",
+        "paths" : [
+          {
+            "editType" : "edit",
+            "file" : "/trunk/OpenAgent/SrcJava/plugins/com.borland.fastxd.integration.xp/src/com/borland/fastxd/integration/xp/functions/AgentClassFunction.java"
+          },
+          {
+            "editType" : "edit",
+            "file" : "/trunk/SilkTestClassic/src/startup/winclass.inc"
+          }
+        ],
+        "revision" : 76695,
+        "user" : "Michael Gehmayr"
+      }
+    ]"""
+
       s"""{
           "lastCompletedBuild": {
             "buildNumber":24838,
             "culprits":[],
-            "changesetItems":[]
+            "changesetItems": $changeSetItems
           },
           "lastSuccessfulBuild": 24838,
           "lastStableBuild": 24838,
@@ -24,6 +54,7 @@ object MockResponseGenerator {
             "building":true
           }
         }"""
+    }
     case "http://lnz-bobthebuilder/hudson/job/SilkTest" =>
       Source.fromFile("nightly-full.txt").getLines.mkString
   }
