63e03f0ead49ef7bac16e2163d9e75d5f4b8378c
Alexander Floh <alexander.floh@gmail.com>
2015-07-30 16:39:15 +0200

    inlined most of the js code as the requirejs loading wasn't done correctly

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,28 +3,13 @@ requirejs.config({
     'jquery' : '../lib/jquery/jquery',
     'react' : '../lib/react/react-with-addons',
     'moment' : '../lib/momentjs/moment',
-    'dashboardSilkTest' : '../jsx/dashboardSilkTest',
-    'dashboardPerformer' : '../jsx/dashboardPerformer',
+    'dashboardSilkTest' : '../jsx/dashboardSilkTest'
   }
 });
 
-define(function(require) {
-  //var config = "";
-  $.ajax({
-    url : '/getConfig',
-    dataType : 'text',
-    async: false,
-  }).done(function(result){
-    config = result;
-  });
-
-  var jQuery = require('jquery');
-  var React = require('react');
-  var dashboard;
-  if (config == "silktest")
-    dashboard = require('dashboardSilkTest');
-  else if (config == "performer")
-    dashboard = require('dashboardPerformer');
-  
-  React.renderComponent(dashboard(), document.getElementById('content'));
-});
+require(['jquery', 'react', 'dashboardSilkTest'],
+  function($, React, Dashboard) {
+    React.renderComponent(Dashboard(), document.getElementById('content'));
+    return function () {};
+  }
+);
--- app/assets/jsx/dashboardSilkTest.jsx
+++ app/assets/jsx/dashboardSilkTest.jsx
@@ -1,9 +1,170 @@
 /** @jsx React.DOM */
 
 define(['react', 'jquery', 'moment'], function(React, $, Moment) {
+  function formatEmplName(name){
+    return name.replace(" ", ".")
+               .replace(/ä/g,"ae")
+               .replace(/ö/g,"oe")
+               .replace(/ü/g,"ue")
+               .replace(/Ä/g,"Ae")
+               .replace(/Ö/g,"Oe")
+               .replace(/Ü/g,"Ue")
+               .replace(/ß/g,"ss") + '.jpg';
+  }
+
+
+  function isUserInProject(user, project){
+    var key = Object.keys(project.data)
+    for (var i = 0; i < project.data[key].members.length; i++)
+      if (project.data[key].members[i] == user.phid)
+        return true;
+    return false;
+  }
+
+  function getAuditsForUser(user, audits, project){
+    var res = 0;
+    if (!isUserInProject(user, project)) return res;
+    for (var i = 0; i < audits.length; i++){
+      if (user.phid == audits[i].auditorPHID &&
+          audits[i].status != "accepted"){
+        res++;
+      }
+    }
+    return res;
+  }
+
+  function getNotAssignedAudits(audits, project){
+    var res = 0;
+    for(var i = 0; i<audits.length; i++){
+        if (audits[i].status === "audit-required" &&
+            audits[i].auditorPHID == Object.keys(project.data)){
+          res++;
+      }
+    }
+    return res;
+  }
+
+  function mergeUserAudits(users, audits, project){
+    var resultArray = new Array();
+    for (var i = 0; i < users.length; i++){
+      var cnt = getAuditsForUser(users[i], audits, project);
+      if (cnt > 0){
+        var userAudit = {
+            userName : users[i].realName,
+            numberOfAudits : cnt
+        };
+        resultArray.push(userAudit);
+      }
+    }
+
+    var userAudit = {
+        userName : "No Auditor",
+        numberOfAudits : getNotAssignedAudits(audits, project)
+    };
+    resultArray.push(userAudit);
+
+    return resultArray;
+  }
+
+  function getDefaultPicture(){
+    return '/assets/images/avatars/default.jpg';
+  }
+
+  var LoadStatusMixin = {
+    getInitialState: function() {
+      return {
+        buildCI: [],
+        buildNightly: {
+          culprits: [],
+          setup: { },
+        },
+        users:[],
+        project:[],
+        audits:[],
+        lastBuild:[],
+        nevergreens:[],
+        employeesAustria:""
+      };
+    },
+
+    // load data from jenkins, austria and phabricator
+    loadStatus: function() {
+      $.ajax({
+        url: '/buildMain',
+        dataType: 'json',
+        success: function(data1) {
+          this.setState(data1);
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+
+      $.ajax({
+          url: '/buildAside',
+          dataType: 'json',
+          success: function(data1) {
+            this.setState(data1);
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+          }.bind(this)
+        });
+
+      $.ajax({
+        url: '/getPhabUser',
+        dataType: 'json',
+        success: function(data1) {
+          this.setState(data1);
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+
+      $.ajax({
+        url: '/getPhabProject',
+        dataType: 'json',
+        success: function(data1) {
+          this.setState(data1);
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+
+      $.ajax({
+        url: '/getPhabAudits',
+        dataType: 'json',
+        success: function(data1) {
+          this.setState(data1);
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+
+      $.ajax({
+        url: '/getUsers',
+        dataType: 'json',
+        success: function(data1) {
+          this.setState(data1);
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
+
+    componentWillMount: function() {
+      this.loadStatus();
+      setInterval(this.loadStatus, this.props.pollInterval);
+    },
+  };
+
 
   var Dashboard = React.createClass({
-    mixins: [getStatusMixin()],
+    mixins: [LoadStatusMixin],
     getDefaultProps: function() {
       return {
         pollInterval: 10000
